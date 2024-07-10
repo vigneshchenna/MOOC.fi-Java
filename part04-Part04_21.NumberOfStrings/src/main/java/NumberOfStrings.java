f0eecd3156c3359ee5b86dc1acf169e9df1f6705
unknown <vigneshchenna02@gmail.com>
2024-07-11 04:00:24 +0530

    completed part 4 excercise 21

--- part04-Part04_21.NumberOfStrings/src/main/java/NumberOfStrings.java
+++ part04-Part04_21.NumberOfStrings/src/main/java/NumberOfStrings.java
@@ -5,6 +5,17 @@ public class NumberOfStrings {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        int count =0;
+        
+        while(true){
+            String strings = scanner.nextLine();
+            if(strings.equals("end")){
+                break;
+            }
+            count++;
+        }
+        System.out.println(count);
 
     }
 }
