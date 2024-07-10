2399d7441981f141adc93a25dca12b773982dc03
unknown <vigneshchenna02@gmail.com>
2024-07-11 04:06:43 +0530

    completed part 4 excercise 22

--- part04-Part04_22.Cubes/src/main/java/Cubes.java
+++ part04-Part04_22.Cubes/src/main/java/Cubes.java
@@ -5,6 +5,13 @@ public class Cubes {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        while(true){
+            String num = scanner.nextLine();
+            if(num.equals("end")){
+                break;
+            }
+            int numm = Integer.valueOf(num);
+            System.out.println(numm*numm*numm);
+        }
     }
 }
