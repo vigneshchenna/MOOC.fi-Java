06f16db2a71b6712165391ac41c4bf606beafd44
unknown <vigneshchenna02@gmail.com>
2024-07-11 03:32:29 +0530

    part 4 excercise 19

--- part04-Part04_19.TelevisionPrograms/src/main/java/Main.java
+++ part04-Part04_19.TelevisionPrograms/src/main/java/Main.java
@@ -8,6 +8,29 @@ public class Main {
 
         ArrayList<TelevisionProgram> programs = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
+        
+        while(true){
+            System.out.println("Name: ");
+            String name = scanner.nextLine();
+            
+            if(name.isEmpty()){
+                break;
+            }
+            
+            System.out.println("Duration: ");
+            int duration = Integer.valueOf(scanner.nextLine());
+            
+            programs.add(new TelevisionProgram(name,duration));
+        }
+        
+        System.out.println("Program's maximum duration? ");
+        int max = Integer.valueOf(scanner.nextLine());
+        
+        for(TelevisionProgram tv:programs){
+            if(tv.getDuration()<=max){
+                System.out.println(tv);
+            }
+        }
 
     }
 }
