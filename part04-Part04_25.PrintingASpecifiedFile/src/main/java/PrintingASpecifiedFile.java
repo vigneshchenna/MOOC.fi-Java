865611fe8cc446d550130fe575b19d047b094f1f
Vignesh <vigneshchenna02@gmail.com>
2024-08-03 13:54:28 +0530

    completed part 4 exercise 25

--- part04-Part04_25.PrintingASpecifiedFile/src/main/java/PrintingASpecifiedFile.java
+++ part04-Part04_25.PrintingASpecifiedFile/src/main/java/PrintingASpecifiedFile.java
@@ -6,6 +6,17 @@ public class PrintingASpecifiedFile {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Which file should have its contents printed?");
+        String fname = scanner.nextLine();
+        
+        try(Scanner scan = new Scanner(Paths.get(fname))){
+            while(scan.hasNextLine()){
+                System.out.println(scan.nextLine());
+            }
+        } catch(Exception e){
+            System.out.println("Error: "+ e.getMessage());
+        }
 
     }
 }
