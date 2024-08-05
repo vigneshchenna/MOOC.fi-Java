34dc84510a059fd8078f815ca265f831f7c51382
Vignesh <vigneshchenna02@gmail.com>
2024-08-05 21:57:30 +0530

    completed part 4 exercise 28

--- part04-Part04_28.NumbersFromAFile/src/main/java/NumbersFromAFile.java
+++ part04-Part04_28.NumbersFromAFile/src/main/java/NumbersFromAFile.java
@@ -13,7 +13,23 @@ public class NumbersFromAFile {
         int lowerBound = Integer.valueOf(scanner.nextLine());
         System.out.print("Upper bound? ");
         int upperBound = Integer.valueOf(scanner.nextLine());
-
+        
+        int count = 0;
+        
+        try(Scanner scan = new Scanner(Paths.get(file))){
+            while(scan.hasNextLine()){
+                int num = Integer.valueOf(scan.nextLine());
+               
+                if(num>= lowerBound && num<= upperBound){
+                   
+                    count++;
+                }
+            }
+        }catch(Exception e){
+            System.out.println("Error: "+ e.getMessage());
+        }
+        
+        System.out.println("Numbers: "+count);
     }
 
 }
