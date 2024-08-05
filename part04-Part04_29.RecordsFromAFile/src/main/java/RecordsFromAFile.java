8dd46f306bb5afb635370adecf3c1b2056a3a9fd
Vignesh <vigneshchenna02@gmail.com>
2024-08-05 22:23:41 +0530

    completed part 4 exercise 29

--- part04-Part04_29.RecordsFromAFile/src/main/java/RecordsFromAFile.java
+++ part04-Part04_29.RecordsFromAFile/src/main/java/RecordsFromAFile.java
@@ -6,6 +6,24 @@ public class RecordsFromAFile {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Name of the file: ");
+        String file = scanner.nextLine();
+        
+        try(Scanner scan = new Scanner(Paths.get(file))){
+            while(scan.hasNextLine()){
+                String line = scan.nextLine();
+                
+                String[] parts = line.split(",");
+                if(Integer.valueOf(parts[1])==1){
+                    System.out.println(parts[0]+", age: "+ parts[1]+ " year");
+                } else{
+                    System.out.println(parts[0]+", age: "+ parts[1]+ " years");
+                }
+            }
+        }catch(Exception e){
+            System.out.println("Error: "+ e.getMessage());
+        }
 
     }
 }
