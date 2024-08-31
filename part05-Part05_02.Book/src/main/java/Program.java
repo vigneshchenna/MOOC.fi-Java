1094c22fa2ef065f2b079cc1ad0c344701b1dc1f
Vignesh <vigneshchenna02@gmail.com>
2024-08-31 19:24:47 +0530

    completed part 5 exercise 2

--- part05-Part05_02.Book/src/main/java/Program.java
+++ part05-Part05_02.Book/src/main/java/Program.java
@@ -9,6 +9,9 @@ public class Program {
 
 //        Book b = new Book("J. K. Rowling", "Harry Potter and the Sorcerer's Stone", 223);
 //        System.out.println(b);
+          Book a = new Book("Jeff Kinney", "Diary of a Wimpy Kid", 154);
+          System.out.println(a);
+          System.out.println(a.getAuthor());
 
     }
 }
