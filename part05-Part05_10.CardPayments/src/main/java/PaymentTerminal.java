cafd3e5594e4b1cebd569c4c88d55df085115b4b
Vignesh <vigneshchenna02@gmail.com>
2024-09-01 20:36:00 +0530

    completed part 5 exercise 10

--- part05-Part05_10.CardPayments/src/main/java/PaymentTerminal.java
+++ part05-Part05_10.CardPayments/src/main/java/PaymentTerminal.java
@@ -9,20 +9,64 @@ public class PaymentTerminal {
 
     public PaymentTerminal() {
         // register initially has 1000 euros of money
+        this.money = 1000;
     }
 
     public double eatAffordably(double payment) {
         // an affordable meal costs 2.50 euros
         // increase the amount of cash by the price of an affordable mean and return the change
         // if the payment parameter is not large enough, no meal is sold and the method should return the whole payment
-        return -1;
+        if(payment>=2.50){
+            affordableMeals++;
+            money+=2.50;
+            return payment-2.50;           
+        }
+        return payment;
+        
     }
 
     public double eatHeartily(double payment) {
         // a hearty meal costs 4.30 euros
         // increase the amount of cash by the price of a hearty mean and return the change
         // if the payment parameter is not large enough, no meal is sold and the method should return the whole payment
-        return -1;
+        if(payment>=4.30){
+            heartyMeals++;
+            money+=4.30;
+            return payment - 4.30;
+        }
+        
+        return payment;
+    }
+    
+    public boolean eatAffordably(PaymentCard card){
+        
+        if(card.balance()>=2.50){
+            affordableMeals++;
+            card.takeMoney(2.50);
+            return true;
+        }
+        
+        return false;
+    }
+    
+    public boolean eatHeartily(PaymentCard card){
+        
+        if(card.balance()>=4.30){
+            heartyMeals++;
+            card.takeMoney(4.30);
+            return true;
+        }
+        
+        return false;
+    }
+    
+    public void addMoneyToCard(PaymentCard card, double sum){
+        if(sum<0){
+            return;
+        }
+        
+        card.addMoney(sum);
+        this.money +=sum;
     }
 
 
