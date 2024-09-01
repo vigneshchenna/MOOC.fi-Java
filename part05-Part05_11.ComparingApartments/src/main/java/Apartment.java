a2a706f976896c6a70bdefa7b1a25fba382f5832
Vignesh <vigneshchenna02@gmail.com>
2024-09-02 01:30:28 +0530

    completed part 5 exercise 11

--- part05-Part05_11.ComparingApartments/src/main/java/Apartment.java
+++ part05-Part05_11.ComparingApartments/src/main/java/Apartment.java
@@ -10,5 +10,27 @@ public class Apartment {
         this.squares = squares;
         this.princePerSquare = pricePerSquare;
     }
+    
+    public boolean largerThan(Apartment compared){
+        if(this.squares > compared.squares){
+            return true;
+        }
+        return false;
+    }
+    
+    public int totalPrice(){
+        return this.squares*this.princePerSquare;
+    }
+    
+    public int priceDifference(Apartment compared){
+        return Math.abs(this.totalPrice()-compared.totalPrice());
+    }
+    
+    public boolean moreExpensiveThan(Apartment compared){
+        if(this.totalPrice()> compared.totalPrice()){
+            return true;
+        }
+        return false;
+    }
 
 }
