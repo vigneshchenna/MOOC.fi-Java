937597efe80a8a4e749b64a351e47c9be257c2ce
Vignesh <vigneshchenna02@gmail.com>
2024-09-02 02:07:58 +0530

    completed part 5 exercise 13

--- part05-Part05_13.IdenticalTwins/src/main/java/Person.java
+++ part05-Part05_13.IdenticalTwins/src/main/java/Person.java
@@ -14,6 +14,23 @@ public class Person {
         this.height = height;
         this.weight = weight;
     }
+    
+    public boolean equals(Object compared){
+        if(this == compared){
+            return true;
+        }
+        
+        if(!(compared instanceof Person)){
+            return false;
+        }
+        
+        Person comparedTo = (Person) compared;
+        
+        if(this.name.equals(comparedTo.name)&& this.birthday.equals(comparedTo.birthday)&&this.height == comparedTo.height && this.weight == comparedTo.weight){
+            return true;
+        }
+        return false;
+    }
 
     // implement an equals method here for checking the equality of objects
 }
