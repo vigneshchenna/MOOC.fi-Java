0aaee51f6971acf647ed3d4145bf247562bc37a8
Vignesh <vigneshchenna02@gmail.com>
2024-09-03 01:22:25 +0530

    completed part 5 exercise 15

---
+++ part05-Part05_15.Archive/src/main/java/Archive.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Vignesh
+ */
+public class Archive {
+    private String identifier;
+    private String name;
+    
+    public Archive(String id, String name){
+        this.identifier = id;
+        this.name = name;
+    }
+    
+    public String getIdentifier(){
+        return this.identifier;
+    }
+    
+    public String getName(){
+        return this.name;
+    }
+    
+    @Override
+    public boolean equals(Object compared){
+        if(this == compared){
+            return true;
+        }
+        
+        if(!(compared instanceof Archive)){
+            return false;
+        }
+        
+        Archive comparedTo = (Archive) compared;
+        
+        return this.identifier.equals(comparedTo.identifier);
+    }
+    
+    
+}
--- part05-Part05_15.Archive/src/main/java/Main.java
+++ part05-Part05_15.Archive/src/main/java/Main.java
@@ -7,6 +7,33 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        ArrayList<Archive> archive = new ArrayList<>();
+
+        while (true) {
+            System.out.println("Identifier? (empty will stop)");
+            String id = scanner.nextLine();
+            if (id.isEmpty()) {
+                break;
+            }
+
+            System.out.println("Name? (empty will stop)");
+            String name = scanner.nextLine();
+            if(name.isEmpty()){
+                break;
+            }
+
+            Archive item = new Archive(id, name);
+
+            if (!(archive.contains(item))) {
+                archive.add(item);
+            }
+
+//            archive.add(new Archive(id,name));
+        }
+        
+        for(Archive items : archive){
+            System.out.println(items.getIdentifier()+": "+items.getName());
+        }
 
     }
 }
