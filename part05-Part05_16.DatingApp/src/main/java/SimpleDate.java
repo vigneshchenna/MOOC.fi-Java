5c1c585097e5a32459e5f6f0a6ae1adb44846f6a
Vignesh <vigneshchenna02@gmail.com>
2024-09-03 01:44:09 +0530

    completed part 5 exercise 16

--- part05-Part05_16.DatingApp/src/main/java/SimpleDate.java
+++ part05-Part05_16.DatingApp/src/main/java/SimpleDate.java
@@ -32,5 +32,37 @@ public class SimpleDate {
 
         return false;
     }
+    
+    public void advance(){
+        this.day++;
+        if(this.day>30){
+            this.day=1;
+            this.month++;
+        }
+        if(this.month>12){
+            this.month = 1;
+            this.year++;
+        }
+    }
+    
+    public void advance(int howManyDays){
+        this.day+=howManyDays;
+        if(this.day>30){
+            this.day = this.day-30;
+            this.month++;
+        }
+        if(this.month>12){
+            this.month=1;
+            this.year++;
+        }
+    }
+    
+    public SimpleDate afterNumberOfDays(int days){
+        SimpleDate pass = new SimpleDate(this.day, this.month, this.year);
+        
+        pass.advance(days);
+        
+        return pass;
+    }
 
 }
