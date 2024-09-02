9ef9f527288cd218695a71d210d286354bcf6ea4
Vignesh <vigneshchenna02@gmail.com>
2024-09-03 02:44:01 +0530

    completed part 5 exercise 17

--- part05-Part05_17.Money/src/main/java/Money.java
+++ part05-Part05_17.Money/src/main/java/Money.java
@@ -32,4 +32,47 @@ public class Money {
         return this.euros + "." + zero + this.cents + "e";
     }
 
+    public Money plus(Money addition) {
+        Money add = new Money(this.euros + addition.euros(), this.cents + addition.cents());
+        return add;
+    }
+
+    public boolean lessThan(Money compared) {
+        if (this.euros() < compared.euros()) {
+            return true;
+        }
+
+        if (this.euros() == compared.euros() && this.cents() < compared.cents()) {
+            return true;
+        }
+        return false;
+    }
+
+    public Money minus(Money decreaser) {
+        int newCents = this.cents;
+        int newEuros = this.euros - decreaser.euros();
+        if (this.cents < decreaser.cents()) {
+            newEuros--;
+            newCents = 100+newCents - decreaser.cents();
+            
+        }
+        
+        
+        //newEuros = newEuros - decreaser.euros();
+
+        if (newEuros < 0) {
+            newEuros = 0;
+        }
+        if (newCents < 0) {
+            newCents = 0;
+        }
+        
+        if(this.lessThan(decreaser)){
+            newEuros =0;
+            newCents =0;
+        }
+        Money sub = new Money(newEuros, newCents);
+        return sub;
+    }
+
 }
