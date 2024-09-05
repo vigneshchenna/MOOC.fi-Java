0e29f74696ccb633255b9da4af070f5b3f07e177
Vignesh <vigneshchenna02@gmail.com>
2024-09-05 21:26:53 +0530

    completed part 6 exercise 3

---
+++ part06-Part06_03.MessagingService/src/main/java/MessagingService.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Vignesh
+ */
+
+import java.util.ArrayList;
+
+public class MessagingService {
+    
+    private ArrayList<Message> msg;
+    public MessagingService(){
+        this.msg = new ArrayList<>();
+    }
+    
+    public void add(Message message){
+        if(message.getContent().length()<=280){
+            msg.add(message);
+        }
+    }
+    
+    public ArrayList<Message> getMessages(){
+        
+        return this.msg;
+    }
+}
