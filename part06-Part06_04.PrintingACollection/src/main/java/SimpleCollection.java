0bfc3576e2d77e3f03ed94ff6b157e6c736f556c
Vignesh <vigneshchenna02@gmail.com>
2024-09-22 02:37:13 +0530

    completed part 6 exercise 4

--- part06-Part06_04.PrintingACollection/src/main/java/SimpleCollection.java
+++ part06-Part06_04.PrintingACollection/src/main/java/SimpleCollection.java
@@ -18,5 +18,30 @@ public class SimpleCollection {
     public ArrayList<String> getElements() {
         return this.elements;
     }
-    
+
+    public void ifEmpty() {
+        if (this.elements.isEmpty()) {
+            System.out.println("The collection characters is empty.");
+        }
+    }
+
+    @Override
+    public String toString() {
+
+        if (this.elements.isEmpty()) {
+            return "The collection " + this.name + " is empty.";
+        }
+        String list = "";
+        for (String item : this.elements) {
+            if (this.elements.size() == 1) {
+                list = list + item;
+            } else {
+                list = list +item+"\n";
+            }
+        }
+        if (this.elements.size() == 1) {
+            return "The collection " + this.name + " has " + this.elements.size() + " element:\n" + elements.get(0);
+        }
+        return "The collection " + this.name + " has " + this.elements.size() + " elements:\n" + list;
+    }
 }
