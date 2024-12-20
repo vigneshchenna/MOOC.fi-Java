c3345cba682dfcc99630a2fbc37bcb8f800ff045
Vignesh <vigneshchenna02@gmail.com>
2024-12-20 11:16:11 +0530

    completed part 7 execise 5

--- part07-Part07_05.Searching/src/main/java/Searching.java
+++ part07-Part07_05.Searching/src/main/java/Searching.java
@@ -44,10 +44,30 @@ public class Searching {
     }
 
     public static int linearSearch(ArrayList<Book> books, int searchedId) {
+        for(int i =0;i<books.size();i++){
+            if(books.get(i).getId() == searchedId){
+                return i;
+            }
+        }
         return -1;
     }
 
     public static int binarySearch(ArrayList<Book> books, long searchedId) {
+        int begin = 0;
+        int end = books.size()-1;
+        while(begin<=end){
+            int middle = (end+begin)/2;
+            
+            if(books.get(middle).getId() == searchedId){
+                return middle;
+            }
+            if(books.get(middle).getId()<searchedId){
+                begin = middle+1;
+            }
+            if(books.get(middle).getId()>searchedId){
+                end = middle-1;
+            }
+        }
         return -1;
     }
 }
